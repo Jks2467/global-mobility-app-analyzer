67cbafdf3ab71425896e85819cfa04b62b2aaaca
Jks2467 <jksharma2467@gmail.com>
2026-03-29 22:30:28 +0530

    reqs installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='visa',
+    version='0.0.1',
+    author='jks',
+    author_email='jksharma2467@gmail.com',
+    packages=find_packages()
+)
